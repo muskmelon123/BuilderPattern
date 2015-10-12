f9d7007a452457a570f92d6b507d322584ec485a
lv.hang <289751112@qq.com>
2015-10-12 16:08:49 +0800

    add note

--- src/Client.java
+++ src/Client.java
@@ -10,3 +10,5 @@ public class Client {
 		System.out.print("skinColor" + actor.getSkinColor());
 	}
 }
+//注意：建造者模式主要用在那种建造的各个部分已经确定。建造的时候可以通过指挥者来控制建造顺序，
+//适合各个模块之间有依赖，因为这样有可能要依赖于建造顺序。如果建造过程比较简单，可以把controller也放入builder里面。
